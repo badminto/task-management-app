0651b5622e9ceaf2bdc27e6683a3baabf2f12ab4
EC2 Default User <ec2-user@ip-172-31-35-84.ap-northeast-1.compute.internal>
2023-01-02 06:00:56 +0000

    遊ぶため

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'tasks/index'
+
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  resources :users  do
+    resources :tasks
+  end
+end
